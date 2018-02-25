4c2dba7dd1e6908d634d159b585d676c0bf32ee2
Luong Vo <vo.tran.thanh.luong@gmail.com>
2018-02-25 09:53:40 +0700

    Add a new workout plans form

--- app/controllers/workout_plans_controller.rb
+++ app/controllers/workout_plans_controller.rb
@@ -1,6 +1,33 @@
 class WorkoutPlansController < ApplicationController
 
   def index
-    @workout_plans = WorkoutPlan.all
+    @workout_plans = WorkoutPlan.order("score DESC")
+  end
+
+  def show
+    @workout_plans = WorkoutPlan.find(params[:id])
+  end
+
+  def new
+    @workout_plans = WorkoutPlan.new
+  end
+
+  def create
+    @workout_plans = WorkoutPlan.new(workout_plans_params)
+    if @workout_plans.save
+      flash[:notice] = "Plan created successfully"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Plan not created successfully"
+      flash[:color] = "invalid"
+    end
+
+    redirect_to "new"
+  end
+
+  private
+
+  def workout_plans_params
+    params.require(workout_plans).permit()
   end
 end
