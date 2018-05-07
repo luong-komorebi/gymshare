95a8fc3687de175699aeea0792b63403e6975d45
Luong Vo <vo.tran.thanh.luong@gmail.com>
2018-05-07 21:12:46 +0700

    Add more function to bot

--- lib/bot/bot_server.rb
+++ lib/bot/bot_server.rb
@@ -9,15 +9,26 @@ class Bot::BotServer
         when Telegram::Bot::Types::CallbackQuery
           bot.logger.warn "I came callback"
           if message.data == 'best'
-            bot.logger.warn "I came"
             bot.api.send_message(parse_mode: 'Markdown', chat_id: message.from.id, text: best_workout)
+          end
+          if message.data == 'yes'
+            bot.logger.warn "I came"
+            bot.api.send_message(parse_mode: 'Markdown', chat_id: message.from.id, text: my_workout(@email))
           end
         else
           case message.text
+          when %r(^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)
+            kb = [
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'No let me insert again', url: 'https://google.com'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Sure', callback_data: 'yes')
+            ]
+            @email = message.text
+            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+            bot.api.send_message(chat_id: message.chat.id, text: "Is that your email?", reply_markup: markup)
           when '/createworkout'
             bot.api.send_message(chat_id: message.chat.id, text: "Sorry this command is yet to be implemented")
           when '/myworkout'
-            bot.api.send_message(chat_id: message.chat.id, text: "Sorry this command is yet to be implemented")
+            bot.api.send_message(chat_id: message.chat.id, text: "Please enter your email")
           when '/help'
             bot.api.send_message(chat_id: message.chat.id, text: "I am here to help you. Type /workout, /myworkout or /createworkout")
           when '/hello'
@@ -25,7 +36,6 @@ class Bot::BotServer
           when '/bye'
             bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
           when '/workout'
-            bot.logger.warn "I came workout"
             kb = [
               Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to app', url: 'https://google.com'),
               Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Get me one workout', callback_data: 'best'),
@@ -55,7 +65,31 @@ class Bot::BotServer
           "\n  _#{exercise.name}_ - #{exercise.description}"\
           "\n  Weight: #{exercise.weight} and Reps: #{exercise.reps}"
         if index != round.exercises.length - 1 && round.rests[index] != nil
-          result << "\n  Then rest for #{round.rests[index].rest_time}"
+          result << "\n  Then rest for #{round.rests[index].rest_time} minutes"
+        end
+      end
+    end
+    result
+  end
+
+  def self.my_workout(email)
+    user = ::User.where(email: email)
+    return "No user found with that email" if !user
+
+    offset = rand(::WorkoutPlan.where(user: user).count)
+    workout_plan = ::WorkoutPlan.where(user: user).offset(offset).first
+    return "No workout plan in database" if !workout_plan
+
+    result = "*I got the best workout for you. Let's start*"
+    rounds = ::Round.where(workout_plan_id: workout_plan.id)
+    rounds.each_with_index do |round, round_index|
+      result << "\nRound _#{round_index}_ : "
+      round.exercises.each_with_index do |exercise, index|
+        result << "\nExercise _#{index}_ :"\
+          "\n  _#{exercise.name}_ - #{exercise.description}"\
+          "\n  Weight: #{exercise.weight} and Reps: #{exercise.reps}"
+        if index != round.exercises.length - 1 && round.rests[index] != nil
+          result << "\n  Then rest for #{round.rests[index].rest_time} minutes"
         end
       end
     end
